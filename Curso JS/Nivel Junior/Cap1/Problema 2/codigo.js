1b268ba33617c6c4bd71450db7d62e9337b86df1
MARTÍN GIL BLANCO <gilblancomartin@gmail.com>
2023-10-20 22:58:20 +0200

    Problema 2, uso de arrays y console.logç

---
+++ Curso JS/Nivel Junior/Cap1/Problema 2/codigo.js
@@ -0,0 +1,57 @@
+let colaDiscoteca = [
+  ["Martín", "H", 23],
+  ["Laura", "M", 15],
+  ["Carlos", "H", 30],
+  ["Ana", "M", 22],
+  ["Juan", "H", 28],
+  ["Sofía", "M", 25],
+  ["David", "H", 31],
+  ["Elena", "M", 17],
+  ["Pedro", "H", 16],
+  ["Isabel", "M", 24],
+];
+let mayoresEdad = [];
+let ganador = [];
+
+const portero = (cola) => {
+  let toret = [];
+  for (persona of cola) {
+    if (persona[2] >= 18) {
+      toret.push(persona);
+    }
+  }
+  return toret;
+};
+
+const ordenAlfabetico = (listaPersonas) => {
+  let toret = [];
+  let listTemporal = [];
+  for (persona of listaPersonas) {
+    listTemporal.push(persona[0]);
+  }
+  listTemporal.sort();
+
+  for (personaOrdenada of listTemporal) {
+    for (persona of listaPersonas) {
+      if (persona[0] == personaOrdenada) {
+        toret.push(persona);
+      }
+    }
+  }
+
+  return toret;
+};
+
+const sorteo = (listaPersonas) => {
+  let posGanador = Math.floor(Math.random() * listaPersonas.length);
+  return listaPersonas[posGanador];
+};
+
+mayoresEdad = portero(colaDiscoteca);
+console.log(mayoresEdad);
+
+mayoresEdad = ordenAlfabetico(mayoresEdad);
+console.log(mayoresEdad);
+
+ganador = sorteo(mayoresEdad);
+console.log(ganador);
---
+++ Curso JS/Nivel Junior/NOTAS.md
@@ -0,0 +1,14 @@
+# Nivel Junior
+
+## Capítulo 1:
+
+Uso de funciones flecha
+Parsear variables
+Uso de prompt y alert
+Bucles y condicionales
+Uso de arrays
+
+## Capítulo 2:
+
+Programación Orientada a Objetos (POO), herencia, abstracción y polimorfismo.
+Uso del DOM
