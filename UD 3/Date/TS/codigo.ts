4290e40c3a4392552718ab0e39604216b3666644
MARTÍN GIL BLANCO <80607504+martin2745@users.noreply.github.com>
2023-11-29 17:54:17 +0100

    UD 3 date TS actualizado

--- UD 3/Date/TS/codigo.ts
+++ UD 3/Date/TS/codigo.ts
@@ -1,30 +1,28 @@
 function diasHastaFinalDeCurso(): number {
-  const hoy = new Date();
-  const finalDeCurso = new Date(hoy.getFullYear() + 1, 5, 25);
+  const hoy: Date = new Date();
+  const finalDeCurso: Date = new Date(hoy.getFullYear() + 1, 5, 25);
 
-  const duracionDia = 1000 * 60 * 60 * 24;
+  const duracionDia: number = 1000 * 60 * 60 * 24;
+
+  console.log("Ejercicio 1");
   console.log("Día actual ", hoy);
   console.log("Final de curso ", finalDeCurso);
-  console.log(
-    "Diferencia en milisegundos",
-    finalDeCurso.getTime() - hoy.getTime()
-  );
+  console.log("Diferencia en milisegundos", finalDeCurso.getTime() - hoy.getTime());
   console.log("Duración de un día en milisegundos", duracionDia);
+
   return Math.ceil((finalDeCurso.getTime() - hoy.getTime()) / duracionDia);
 }
 
 console.log("Días restantes hasta final de curso: " + diasHastaFinalDeCurso());
 
-/*******************************************************************************/
-
 function aniversarioEnFinDeSemana(): number[] {
-  const tuAniversario = new Date("2023-10-31");
-
-  const inicioAnho = new Date();
+  const tuAniversario: Date = new Date("2023-10-31");
+  const inicioAnho: Date = new Date();
   inicioAnho.setMonth(0);
   inicioAnho.setDate(1);
 
   const anos: number[] = [];
+
   while (inicioAnho.getFullYear() <= 2100) {
     if (tuAniversario.getDay() === 0 || tuAniversario.getDay() === 6) {
       anos.push(inicioAnho.getFullYear());
@@ -36,49 +34,52 @@ function aniversarioEnFinDeSemana(): number[] {
   return anos;
 }
 
+console.log("\nEjercicio 2");
 console.log(
   "Años en los que tu aniversario caerá en fin de semana: " +
     aniversarioEnFinDeSemana()
 );
 
-/*******************************************************************************/
+function mostrarFechaEnFormato3(formato: string): void {
+  const opcionesFecha = { day: 'numeric', month: 'numeric', year: 'numeric' };
+  const opcionesDiaSemana = { weekday: 'long' };
+  const opcionesMesCompleto = { month: 'long' };
 
-function formatoFecha(opcion: number): string {
-  const fechaActual = new Date();
-  switch (opcion) {
-    case 1:
-      return fechaActual.toLocaleDateString();
-    case 2:
-      return fechaActual.toLocaleString("es-ES", {
-        weekday: "long",
-        year: "numeric",
-        month: "long",
-        day: "numeric",
-      });
-    case 3:
-      return fechaActual.toLocaleString("en-US", {
-        weekday: "long",
-        year: "numeric",
-        month: "long",
-        day: "numeric",
-      });
+  const fechaActual: Date = new Date();
+
+  switch (formato) {
+    case '1':
+      console.log(fechaActual.toLocaleDateString('es-ES', opcionesFecha));
+      break;
+    case '2':
+      const diaSemana = fechaActual.toLocaleDateString('es-ES', opcionesDiaSemana);
+      const dia = fechaActual.getDate();
+      const mesCompleto = fechaActual.toLocaleDateString('es-ES', opcionesMesCompleto);
+      const año = fechaActual.getFullYear();
+      console.log(`${diaSemana}, ${dia} de ${mesCompleto} de ${año}`);
+      break;
+    case '3':
+      const dayOfWeek = fechaActual.toLocaleDateString('en-US', { weekday: 'long' });
+      const month = fechaActual.toLocaleDateString('en-US', { month: 'long' });
+      const day = fechaActual.getDate();
+      const year = fechaActual.getFullYear();
+      console.log(`${dayOfWeek}, ${month} ${day}, ${year}`);
+      break;
     default:
-      return "Opción no válida";
+      console.log('Formato no válido. Introduce 1, 2 o 3.');
   }
 }
 
-console.log(formatoFecha(1)); // Formato 1
-console.log(formatoFecha(2)); // Formato 2
-console.log(formatoFecha(3)); // Formato 3
+// Prueba del programa
+const formatoUsuario3: string = prompt('Introduce el formato \n1 17/02/2016, \n2 Mércores, 17 de febreiro de 2016 \n3 Wednesday, February 17, 2016');
+mostrarFechaEnFormato3(formatoUsuario3);
 
-/*******************************************************************************/
-
-function formatoHora(opcion: number): string {
-  const horaActual = new Date();
+function formatoHora(opcion: string): string {
+  const horaActual: Date = new Date();
   switch (opcion) {
-    case 1:
+    case '1':
       return horaActual.toLocaleTimeString();
-    case 2:
+    case '2':
       return horaActual.toLocaleString("en-US", {
         hour: "2-digit",
         minute: "2-digit",
@@ -88,5 +89,5 @@ function formatoHora(opcion: number): string {
   }
 }
 
-console.log(formatoHora(1)); // Formato 1
-console.log(formatoHora(2)); // Formato 2
+const formatoUsuario4: string = prompt('Introduce el formato \n1 17/02/2016, \n2 Mércores, 17 de febreiro de 2016 \n3 Wednesday, February 17, 2016');
+console.log(formatoHora(formatoUsuario4));
